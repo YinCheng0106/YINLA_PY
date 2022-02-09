f315f18a562c484669ebc850c91ebd02cb92fbb9
YinCheng0106 <eason010630@gmail.com>
2022-02-09 12:50:01 +0800

    Update Bot.py

---
+++ Bot.py
@@ -0,0 +1,28 @@
+from re import T
+import discord
+intents = discord.Intents.all()
+
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='YIN',intents=intents)
+
+@bot.event
+async def on_ready():
+    print(">>bot is online<<")
+
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(940826399227400222)
+    await channel.send(f'**{member}** join!')
+
+@bot.event
+async def on_member_remove(member):
+    channel = bot.get_channel(940826399227400222)
+    await channel.send(f'**{member}** leave...')
+
+@bot.command()
+async def ping(ctx): 
+    await ctx.send(f'{round(bot.latency*1000)} (ms)')
+
+
+bot.run('OTQwNzc4NzQzNjM3NjA2NDUx.YgMWew._UGW_Ub2Xve4l3ctstkRJMRUJLo')
